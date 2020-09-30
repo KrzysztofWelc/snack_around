af63d562d6c6bce2be76eca0f7a306a5f6641ef2
krzysztofwelc <krzysiekwelc18@gmail.com>
2020-09-30 19:02:20 +0200

    quick fix

--- snack_around/snack_around/settings.py
+++ snack_around/snack_around/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'PORT': config('DB_PORT_DEV')
     }
 }
-if DEBUG:
+if not DEBUG:
     import dj_database_url
     db_from_env = dj_database_url.config(conn_max_age=600)
     DATABASES['default'].update(db_from_env)
